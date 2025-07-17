e7eccf390118a79dcc4ded60d57c8f2744f2ed7e
Lukianov Illia <Lukjanovilly@gmail.com>
2025-07-17 14:45:02 +0300

    finish task

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,4 +1,5 @@
-import { getProducts } from "../services/products.js";
+import createHttpError from "http-errors";
+import { createProduct, deleteProduct, getProductById, getProducts, patchProduct } from "../services/products.js";
 
 export const getProductsController = async (req, res) => {
   const products = await getProducts();
@@ -7,4 +8,52 @@ export const getProductsController = async (req, res) => {
     message: "Successfully found products!",
     data: products,
   });
+};
+
+export const getProductByIdController = async (req, res) => {
+  const product = await getProductById(req.params.productId);
+
+  if (product === null) {
+    throw new createHttpError.NotFound('Product not found');
+  };
+
+  return res.json({
+    status: 200,
+    message: 'Successfully found products!',
+    data: product
+  });
+};
+
+export const createProductController = async (req, res) => {
+  const product = await createProduct(req.body);
+
+  return res.status(201).json({
+       status: 201,
+       message: "Successfully created a product!",
+       data: product
+   });
+};
+
+export const patchProductController = async (req, res) => {
+  const product = await patchProduct(req.params.productId, req.body);
+
+  if (product === null) {
+   throw new createHttpError.NotFound('Product not found');
+  };
+
+  return res.json({
+    status: 200,
+    message: "Successfully patched a product!",
+    data: product,
+  });
+};
+
+export const deleteProductController = async (req, res) => {
+  const product = await deleteProduct(req.params.productId);
+
+  if (product === null) {
+   throw new createHttpError.NotFound('Product not found');
+  };
+
+  return res.sendStatus(204);
 };
--- src/services/products.js
+++ src/services/products.js
@@ -1,3 +1,11 @@
 import { Product } from "../db/schemas/Product.js";
 
 export const getProducts = () => Product.find();
+
+export const getProductById = (id) => Product.findById(id);
+
+export const createProduct = (data) => Product.create(data);
+
+export const patchProduct = (id, data) => Product.findByIdAndUpdate(id, data, {new: true});
+
+export const deleteProduct = (id) => Product.findByIdAndDelete(id);
